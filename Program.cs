b98e61bec7df72c6abcaa2481b9ff4bf84e43561
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-09 18:52:49 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,85 +1,67 @@
-﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
-//каждой строки двумерного массива.
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с 
+//наименьшей суммой элементов.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
 //8 4 2 4
-//В итоге получается вот такой массив:
-//7 4 2 1
-//9 5 3 2
-//8 4 4 2
+//5 2 6 7
 
 using System;
 using static System.Console;
 
 Clear();
 
-int rowCount = 3;
-int colCount = 4;
-int[,] arr = GenerateArray(rowCount, colCount);
-WriteLine("Исходный массив: ");
-PrintArray(arr);
-WriteLine("Сортировка по строкам: ");
-int[] row = new int[colCount];
 
-for (int i = 0; i < rowCount; i++)
+
+int[,] array = new int[4,4];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < colCount; j++)
-    row[j] = arr[i, j];
-    BubbleSort(row);
-    Insert(true, i, row, arr);
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
 }
 
-PrintArray(arr);
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
 
-void Insert(bool isRow, int dim, int[] source, int[,] dest)
+
+int SumLineElements(int[,] array, int i)
 {
-    for (int k = 0; k < source.Length; k++)
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        if (isRow)
-        dest[dim, k] = source[k];
-        else
-         dest[k, dim] = source[k];
+      array[i, j] = new Random().Next(0,10);
     }
+  }
 }
 
-int[,] GenerateArray(int t, int i)
+void WriteArray (int[,] array)
 {
-    int[,] table = new int[t, i];
-    Random random = new Random();
-    for (int a = 0; a < t; a++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int b = 0; b < i; b++)
-        {
-            table[a, b] = random.Next(0, 9);
-        }
+      Console.Write(array[i,j] + " ");
     }
-    return table;
+    Console.WriteLine();
+  }
 }
-
-void PrintArray(int[,] array)
-{
-    for (int a = 0; a < array.GetLength(0); a++)
-    {
-        for (int b = 0; b < array.GetLength(1); b++)
-        {
-            Write(array[a, b] + " ");
-        }
-        WriteLine();
-    }
-}
-
-void BubbleSort(int[] inArray)
-{
-    for (int i = 0; i < inArray.Length; i++)
-    for (int j = 0; j < inArray.Length - i - 1; j++)
-    {
-     if (inArray[j] > inArray[j + 1])
-     {
-        int temp = inArray[j];
-        inArray[j] = inArray[j + 1];
-        inArray[j + 1] = temp;
-     }
-    }
-}
-    
